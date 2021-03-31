8f267c8f5989dd3b2036741a653760edaf924336
StilianB <stilianb@gmail.com>
2021-03-31 15:13:49 -0600

    Average restructuring

--- CS1030/Project_3_Average_Postal/StilianBalasopoulov_03_Average.py
+++ CS1030/Project_3_Average_Postal/StilianBalasopoulov_03_Average.py
@@ -5,39 +5,45 @@ usrList = []
 avgList = []
 higher = []
 lower = []
-average = 0
 
-# User Input
-while True:
-	usrInput = input("> ")
+def inputValidation(usrList):
+	while True:
+		usrInput = input("> ")
 
-	if not usrInput or usrInput == '0':
-		break
-	else:
-		try:
-			usrInput = int(usrInput)
-			usrList.append(usrInput)
-		except ValueError:
-			print("Input was not an int")
+		if not usrInput or usrInput == '0':
+			break
+		else:
+			try:
+				usrInput = int(usrInput)
+				usrList.append(usrInput)
+			except ValueError:
+				print("Input was not an int")
 
-# Find Average
-try:
-	average = int(sum(usrList) / len(usrList))
-except ZeroDivisionError:
-	pass
+def findAvg(usr):
+	try:
+		average = int(sum(usr) / len(usr))
+	except ZeroDivisionError:
+		average = 0
 
-# Sort into lists
-for var in usrList:
-	if var == average:
-		avgList.append(var)
-	elif var < avg:
-		lower.append(var)
-	else:
-		higher.append(var)
+	return average
+
+def sortList(usr, avg, higher, lower):
+	for var in usr:
+		if var == average:
+			avg.append(var)
+		elif var < average:
+			lower.append(var)
+		else:
+			higher.append(var)
+
+# Operations
+inputValidation(usrList)
+average = findAvg(usrList)
+sortList(usrList, avgList, higher, lower)
 
 # Output
 print("\nUser inputted list: " + str(usrList))
-print("Average: " + str(avg))
+print("Average: " + str(average))
 print("\nHigher: " + str(higher))
 print("Lower: " + str(lower))
 print("Average: " + str(avgList))
