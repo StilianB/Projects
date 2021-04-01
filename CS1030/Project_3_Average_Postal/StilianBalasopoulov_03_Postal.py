c363d7526b631ea711954c2af4c6d9a238d13065
StilianB <stilianb@gmail.com>
2021-04-01 13:24:11 -0600

    Postal baseline

--- CS1030/Project_3_Average_Postal/StilianBalasopoulov_03_Postal.py
+++ CS1030/Project_3_Average_Postal/StilianBalasopoulov_03_Postal.py
@@ -1,3 +1,19 @@
+"""
+Odd characters are letters
+Even characters are numbers
+
+check the first value (userInput[0]) is in postalCodes
+	if not then output invalid input
+
+check the second value if its 0 or otherwise
+0 is rural
+anything else is urban
+
+Input
+validate the input - if invalid then print why
+if user hits enter end the program
+"""
+# Variables
 postalCodes = {
 	'A': 'Newfoundland',
 	'B': 'Nova Scotia',
@@ -15,22 +31,51 @@ postalCodes = {
 	'S': 'Saskatchewan',
 	'T': 'Albera',
 	'V': 'British Columbia',
-	'X': 'Nunavut',
-	'x': 'Northwest Territories',
+	'X': 'Nunavut or Northwest Territories',
 	'Y': 'Yukon'
 }
+isUrban = False
+isRural = False
 
-userInputList = []
-
+# Input / Output
 while True:
-	userInput = input("Enter your postal code: ").upper()
+	userInput = input("Enter a valid Postal Code: ").upper()
 
 	if not userInput:
 		break
 
-	if userInput[0] in postalCodes:
-		print(postalCodes[userInput[0]])
-		userInputList.append(userInput)
+	# Checking first character
+	if len(userInput) != 7:
+		print("Length does not equal 7")
+	elif userInput[0] not in postalCodes:
+		print("Not a valid location")
+
+	# Checking second character for a 0
+	if userInput[1] == '0':
+		isRural = True
+		isUrban = False
 	else:
-		print("First letter doesn't reference a province.")
-print(userInputList)
+		isRural = False
+		isRural = True
+
+	# Determine if format of the whole string is correct
+
+	# Output
+	print("Location: " + postalCodes[userInput[0]])
+
+	if isRural:
+		print("Location is Rural")
+	elif isUrban:
+		print("Location is Urban")
+
+"""	for val in userInput:
+		if val % 2 == 0:
+			if val.isdigit():"""
+
+
+
+
+
+
+
+
