32f096648721cfe7d9d63e2e7f85dc24e14625c7
StilianB <stilianb@gmail.com>
2021-04-15 09:16:27 -0600

    First Draft of Scrabble

--- CS1030/Project_4_Scrabble_Frequencies_Concatenate/StilianBalasopoulov_04_Scrabble.py
+++ CS1030/Project_4_Scrabble_Frequencies_Concatenate/StilianBalasopoulov_04_Scrabble.py
@@ -1,8 +1,64 @@
-"""Letters	# points
-A, E, I, L, N, O, R, S, T, U	1
-D, G	2
-B, C, M, P	3
-F, H, V, W, Y	4
-K	5
-J, X	8
-Q, Z	10"""
+pointTable = {
+	'A': 1,
+	'B': 3,
+	'C': 3,
+	'D': 2,
+	'E': 1,
+	'F': 4,
+	'G': 2,
+	'H': 4,
+	'I': 1,
+	'J': 8,
+	'K': 5,
+	'L': 1,
+	'M': 3,
+	'N': 1,
+	'O': 1,
+	'P': 3,
+	'Q': 10,
+	'R': 1,
+	'S': 1,
+	'T': 1,
+	'U': 1,
+	'V': 4,
+	'W': 4,
+	'X': 8,
+	'Y': 4,
+	'Z': 10
+}
+scores = []
+
+def findScore(words, points, scores):
+	for line in words:
+		line = line.upper()
+		score = 0
+
+		if not line:
+			continue
+
+		for char in line:
+			if char in points and char.isalpha():
+				score += points[char]
+		scores.append(score)
+	
+	return scores
+
+with open("1030 Project 04 01 Words.txt") as input:
+	words = input.read().splitlines()
+
+for word in words:
+	if not word:
+		words.remove(word)
+
+scores = findScore(words, pointTable, scores)
+
+print("{:^15}|{:^15}".format("Words","Points"))
+print("-------------------------------")
+
+for i in range(len(words) - 1):
+	print("{:<15}|{:>15}".format(str(words[i]), str(scores[i])))
+
+print("-------------------------------")
+print("{:^15}|{:^15}".format("Total:",str(sum(scores))))
+
+
